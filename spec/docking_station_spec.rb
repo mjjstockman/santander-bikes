433cc5de868693bc8a5f1a1af6daa29cde527cf5
Matt Stockman <mjjstockman@gmail.com>
2019-10-31 20:57:00 +0000

    refractor docking_station_spec to use bike doubles and stubs

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,12 +3,13 @@ require 'bike'
 
 describe DockingStation do
   subject(:docking_station) { described_class.new }
+  let(:bike) { double('bike') }
 
   describe '#release_bike' do
     context 'docking station has a docked bike' do
       it 'releases a bike' do
-        bike = Bike.new
-        subject.dock(bike)
+        docking_station.dock(bike)
+        allow(bike).to receive(:working?).and_return(true)
 
         expect(bike).to be_working
       end
@@ -19,13 +20,24 @@ describe DockingStation do
         expect { docking_station.release_bike }.to raise_error('No docked bikes')
       end
     end
+
+    context 'does not release a broken bike' do
+      it 'raises an error' do
+        docking_station.dock(bike)
+
+        allow(bike).to receive(:working?).and_return(false)
+
+        expect { docking_station.release_bike }.to raise_error 'No working bikes'
+      end
+    end
   end
 
   describe '#dock' do
     context 'docking station has space' do
       it 'returns the docked bike' do
-        bike = Bike.new
-        subject.dock(bike)
+        docking_station.dock(bike)
+
+        allow(bike).to receive(:working?).and_return(true)
 
         expect(docking_station.release_bike).to eq bike
       end
@@ -33,9 +45,9 @@ describe DockingStation do
 
     context 'docking station is at capacity' do
       it '#dock raises an error' do
-        docking_station.capacity.times { docking_station.dock(Bike.new) }
+        docking_station.capacity.times { docking_station.dock(bike) }
 
-        expect { docking_station.dock(Bike.new) }.to raise_error 'Docking Station full'
+        expect { docking_station.dock(bike) }.to raise_error 'Docking Station full'
       end
     end
   end
